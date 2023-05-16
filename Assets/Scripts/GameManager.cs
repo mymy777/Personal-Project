1f193b2bb9c8701bb10c4403db2904ade333a9e0
eronmoran <ermoran@s.sfusd.edu>
2023-05-16 13:52:36 -0700

    added title and button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-   
+    
     }
 
     // Update is called once per frame
     void Update()
     {
        
+    }
+
+    public void StartGame()
+    {
+        titleScreen.gameObject.SetActive(false);
     }
 }
